dd3652c94e3e83bf74095396401da22dad419149
Niklas Berggren <niklas.berggren@lumenradio.com>
2022-06-29 12:07:32 +0200

    Clean up, split nvim setup out into separate repo

--- config/nvim/lua/plugin-settings/lsp/server-settings/jedi_language_server.lua
+++
@@ -1,8 +0,0 @@
-local util = require('lspconfig.util')
-local common = require("plugin-settings.lsp.server-settings.common_functions")
-
-return {
-  on_attach = common.default_attach,
-
-  root_dir = util.root_pattern('.git')
-}
--- config/qtile/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-picom -b --conf ${HOME}/.config/picom/picom.conf
-setxkbmap -layout se
-xset r rate 175 35
