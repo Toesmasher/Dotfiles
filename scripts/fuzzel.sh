6b4b07832bafe4b5c8bb2a42e91ac7e698eeeeb6
Niklas Berggren <nicke@toesmasher.se>
2022-11-07 13:38:29 +0100

    Add Fira Code font

--- scripts/fuzzel.sh
+++ scripts/fuzzel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Colors from gruvbox.nvim
-FONT="Hack Nerd Font Mono"
+FONT="Fira Code"
 BACKGROUND_COLOR=32302f99 # dark0_soft
 TEXT_COLOR=f2e5bcff       # light0_soft
 SELECTION_COLOR=d65d0eff  # neutral_orange
