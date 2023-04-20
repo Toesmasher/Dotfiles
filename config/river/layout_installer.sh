a6d657805d95c54e2d2c7c0a13368a6b8bd79310
Niklas Berggren <nicke@toesmasher.se>
2023-04-20 11:19:11 +0200

    Complete move to Hyprland

--- config/river/layout_installer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-pip install --user pywayland
-python -m pywayland.scanner -i /usr/share/wayland/wayland.xml /usr/share/river-protocols/river-layout-v3.xml
-
--- scripts/startriver.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# export WLR_NO_HARDWARE_CURSORS=1
-# export SDL_VIDEODRIVER=wayland
-# export WLR_RENDERER=vulkan
-export XKB_DEFAULT_LAYOUT=se
-
-river
