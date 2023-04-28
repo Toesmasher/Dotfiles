464b829a18df12660ffeb9f0be2d91cddeafdb46
Niklas Berggren <nicke@toesmasher.se>
2023-04-28 14:27:53 +0200

    Catppuccin tmux

---
+++ config/tmux/themes/catppuccin/update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/catppuccin/tmux/main/catppuccin.tmux -o catppuccin.tmux --silent
+curl https://raw.githubusercontent.com/catppuccin/tmux/main/catppuccin-frappe.tmuxtheme -o catppuccin-frappe.tmuxtheme --silent
+curl https://raw.githubusercontent.com/catppuccin/tmux/main/catppuccin-latte.tmuxtheme -o catppuccin-latte.tmuxtheme --silent
+curl https://raw.githubusercontent.com/catppuccin/tmux/main/catppuccin-macchiato.tmuxtheme -o catppuccin-macchiato.tmuxtheme --silent
+curl https://raw.githubusercontent.com/catppuccin/tmux/main/catppuccin-mocha.tmuxtheme -o catppuccin-mocha.tmuxtheme --silent
