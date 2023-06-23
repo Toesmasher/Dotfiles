0b7d98010e5d6d5c6daf9001a5ff3173941c3c9b
Niklas Berggren <nicke@toesmasher.se>
2023-06-23 11:38:38 +0200

    Replace NNN with xplr

---
+++ config/xplr/init.lua
@@ -0,0 +1,10 @@
+version = "0.21.2"
+
+local home = os.getenv("HOME")
+package.path = home
+.. "/.config/xplr/plugins/?/init.lua;"
+.. home
+.. "/.config/xplr/plugins/?.lua;"
+.. package.path
+
+require("tri-pane").setup()
