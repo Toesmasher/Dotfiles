93a805e0319f753aab16f310e7f185f5c4c81136
Niklas Berggren <nicke@toesmasher.se>
2023-06-26 09:39:26 +0200

    Add keybindings to xplr for opening

--- config/xplr/init.lua
+++ config/xplr/init.lua
@@ -8,3 +8,25 @@ package.path = home
 .. package.path
 
 require("tri-pane").setup()
+
+xplr.config.modes.builtin.default.key_bindings.on_key["e"] = {
+  help = "open with nvim",
+  messages = {
+    {
+      BashExec0 = [===[
+        nvim "${XPLR_FOCUS_PATH:?}"
+      ]===]
+    }
+  }
+}
+
+xplr.config.modes.builtin.default.key_bindings.on_key["o"] = {
+  help = "open with xdg",
+  messages = {
+    {
+      BashExecSilently0 = [===[
+        xdg-open "${XPLR_FOCUS_PATH:?}"
+      ]===]
+    }
+  }
+}
