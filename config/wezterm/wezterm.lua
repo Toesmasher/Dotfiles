f67397a55f12d77246822a5014417b4f20546b5b
Niklas Berggren <nicke@toesmasher.se>
2023-07-25 11:32:18 +0200

    Replace kitty with wezterm, fix font names

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require('wezterm')
+
+return {
+  color_scheme = 'Catppuccin Macchiato',
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.95,
+
+  font_size = 10.0,
+  font = wezterm.font('FiraCode Nerd Font'),
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font('FiraCode Nerd Font', { italic = true, weight = 'DemiBold' }),
+    },
+    {
+      italic = true,
+      intensity = 'Bold',
+      font = wezterm.font('FiraCode Nerd Font', { italic = true, weight = 'Bold' }),
+    },
+    {
+      intensity = 'Bold',
+      font = wezterm.font('FiraCode Nerd Font', { weight = 'Bold' }),
+    },
+  },
+}
