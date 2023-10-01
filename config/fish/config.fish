c916eef46ed3c24ef8c6407b971ea52a3b8847de
Niklas Berggren <nicke@toesmasher.se>
2023-10-02 00:39:27 +0200

    Add fish

---
+++ config/fish/config.fish
@@ -0,0 +1,6 @@
+source $HOME/.config/fish/exports.fish
+source $HOME/.config/fish/paths.fish
+
+if test -f $HOME/.fish.local
+  source $HOME/.fish.local
+end
---
+++ config/fish/exports.fish
@@ -0,0 +1,10 @@
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+
+if type -q nvim
+  set -gx PAGER "nvim +Man!"
+  set -gx EDITOR "nvim"
+end
+
+if type -q brave
+  set -gx BROWSER "brave"
+end
