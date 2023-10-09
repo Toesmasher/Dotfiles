d225a1c03572884db40ad5321cfb22dba37d4a57
Niklas Berggren <nicke@toesmasher.se>
2023-10-09 14:09:18 +0200

    Wrong logic on sshagent, because I'm a genious

--- config/fish/sshagent.fish
+++ config/fish/sshagent.fish
@@ -5,7 +5,7 @@ switch (uname -s)
   case '*'
     set -gx SSH_AUTH_SOCK /tmp/$USER-ssh-agent.socket
 
-    if test -f $SSH_AUTH_SOCK
+    if test ! -f $SSH_AUTH_SOCK
       ssh-agent -a $SSH_AUTH_SOCK > /dev/null 2>&1
     end
 end
