85e9cb17660ade7f66e48980e5837498b88a1661
Niklas Berggren <nicke@toesmasher.se>
2024-02-25 22:55:43 +0100

    Default wezterm to tmux

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,6 +5,8 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   window_background_opacity = 0.85,
 
+  default_prog = { 'tmux' },
+
   font_size = 10.0,
   font = wezterm.font('FiraCode Nerd Font'),
   font_rules = {
