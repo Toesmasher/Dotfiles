1137489dcefaeb073b27244b746f89a45ac8addc
Niklas Berggren <nicke@toesmasher.se>
2024-09-19 09:30:27 +0200

    Add extra args to qemu-start.sh

--- scripts/qemu-start.sh
+++ scripts/qemu-start.sh
@@ -5,7 +5,8 @@ set -e
 # Simple machine starter
 
 MACHINE_DIR="$1"
-ISO_FILE="$2"
+shift
+EXTRA_ARGS="$@"
 
 DISK_FILE="${MACHINE_DIR}/disk.qcow2"
 OVMF_VARS_FILE="${MACHINE_DIR}/OVMF_VARS.fd"
@@ -32,16 +33,15 @@ if [[ ! -f "${OVMF_VARS_FILE}" ]]; then
   exit 1
 fi
 
-if [[ ! -z "${ISO_FILE}" && ! -f "${ISO_FILE}" ]]; then
-  echo "ISO image ${ISO_FILE} not found"
-  exit 1
-fi
-
 QEMU_ARGS="--enable-kvm -M q35 "
 QEMU_ARGS+="-m 4096 -cpu host -smp 4,sockets=1,cores=4,threads=1 "
 QEMU_ARGS+="-vga virtio -display gtk,gl=on "
 QEMU_ARGS+="-drive if=pflash,format=raw,readonly=on,file=${OVMF_CODE_FILE} "
 QEMU_ARGS+="-drive if=pflash,format=raw,file=${OVMF_VARS_FILE} "
+
+# USB
+QEMU_ARGS+="-usb "
+QEMU_ARGS+="-device usb-tablet "
 
 # Emulate NVMe for the disk
 QEMU_ARGS+="-drive file="${DISK_FILE}",if=none,id=disk "
@@ -50,9 +50,6 @@ QEMU_ARGS+="-device nvme,serial=12345678,drive=disk "
 # Regular drive
 # QEMU_ARGS+="-drive file=${DISK_FILE} "
 
-if [[ ! -z "${ISO_FILE}" && -f "${ISO_FILE}" ]]; then
-  echo "Using ISO file ${ISO_FILE} for cdrom"
-  QEMU_ARGS+="-cdrom ${ISO_FILE} "
-fi
+QEMU_ARGS+=" ${EXTRA_ARGS}"
 
 qemu-system-x86_64 ${QEMU_ARGS} 
