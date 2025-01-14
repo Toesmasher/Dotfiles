ffe711846a7a5e0fd411eefc4e8e22021532a5ca
Niklas Berggren <nicke@toesmasher.se>
2025-01-14 10:20:02 +0100

    Fix export

--- config/fish/exports.fish
+++ config/fish/exports.fish
@@ -2,7 +2,7 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx ELECTRON_OZONE_PLATFORM_HINT "wayland"
 
 set -gx BAT_STYLE "plain"
-set -gx BAT_THEME="Catppuccin Macchiato"
+set -gx BAT_THEME "Catppuccin Macchiato"
 
 if type -q nvim
   set -gx MANPAGER "nvim +Man!"
