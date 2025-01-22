5518a12c527e203631abcd9a91659bed13b927ad
Niklas Berggren <nicke@toesmasher.se>
2025-01-22 09:49:00 +0100

    Set i386 as default ant build

--- config/fish/functions/i4m_setup.fish
+++ config/fish/functions/i4m_setup.fish
@@ -14,7 +14,7 @@ function i4m_setup
   set -gx PATH {$ANT_HOME}/bin:{$GROOVY_HOME}/bin:{$HOME}/bin:{$ASX_TOOL_DIR}/bin:{$QT_HOME}/target/linux/i386/bin:{$PATH}
   set -gx LD_LIBRARY_PATH {$QT_HOME}/target/linux/i386/lib/:{$OMNI_HOME}/target/linux/i386/lib:$LD_LIBRARY_PATH 
 
-  alias ant="ant -v -DapplyOnRequiredComponents=compile"
+  alias ant="ant -v -DapplyOnRequiredComponents=compile -Dtarget.type=i386 -Dtarget.os.arch=i386 -Dtarget.os.version=26 -Dtarget.os.name=linux"
   alias ant_mx4="ant -v -DapplyOnRequiredComponents=compile -Dtarget.type=MX4 -Dtarget.os.arch=armv7 -Dtarget.os.version=26 -Dtarget.os.name=linux"
   alias ant_cvc="ant -v -DapplyOnRequiredComponents=compile -Dtarget.type=CVC -Dtarget.os.arch=aarch64 -Dtarget.os.version=26 -Dtarget.os.name=linux"
 end
