af4d78152044c35ea8e63dc6cb80271f234deb14
Niklas Berggren <nicke@toesmasher.se>
2026-01-03 00:01:21 +0100

    Slight fish tweaks

--- config/fish/functions/yazi.fish
+++ config/fish/functions/yazi.fish
@@ -1,8 +1,8 @@
 function yazi
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	y=(which yazi) $y $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+  set tmp (mktemp -t "yazi-cwd.XXXXXX")
+  y=(which yazi) $y $argv --cwd-file="$tmp"
+  if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+    builtin cd -- "$cwd"
+  end
+  rm -f -- "$tmp"
 end
